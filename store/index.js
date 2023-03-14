8c30e6551e9344eec162a9e20adc7365408b6c14
tylergrez <tyler.grzegorek@gmail.com>
2023-03-14 15:47:22 -0700

    Fixed API call

--- store/index.js
+++ store/index.js
@@ -1,5 +1,5 @@
 export { default as About } from "./About";
-export { default as Search } from "./EventSearch";
+export { default as Events } from "./Events";
 export { default as Contact } from "./Contact";
 export { default as Home } from "./Home";
 export { default as Links } from "./Links";
