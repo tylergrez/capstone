5305562c978450fadf795c0d46ea008b3849fc3b
tylergrez <tyler.grzegorek@gmail.com>
2023-03-20 17:40:21 -0700

    for Michael

--- components/views/blankPage.js
+++ components/views/blankPage.js
@@ -135,3 +135,24 @@ Promise.allSettled([promiseTM, promiseSG])
           //             done();
           //           }
           //         );
+
+
+const search("") {
+  var input, filter, table, tr, td, i, txtValue ;
+  input = document.getElementsByName("event-input");
+  table = document.getElementById("resultsTable");
+  tr = table.getElementsByTagName("tr");
+  filter = input.value.toUpperCase();
+
+  for (i=0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
