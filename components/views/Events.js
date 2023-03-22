63eaf7cfcc6581ca9a676b7a6e0afd1f8ce53715
tylergrez <tyler.grzegorek@gmail.com>
2023-03-21 19:03:01 -0700

    added event table sort

--- components/views/Events.js
+++ components/views/Events.js
@@ -1,4 +1,5 @@
 import html from "html-literal";
+import { isNaN } from "lodash";
 // import tmLogo from "../../assets/img/Ticketmaster-Emblem.png";
 
 function randomSection() {
@@ -47,6 +48,105 @@ function fixSGDateTime(string) {
   return date + "<br>" + time;
 }
 
+// var tables = document.querySelectorAll("table.sortable"), table, thead, headers, i, j;
+// for (i = 0; i < tables.length; i++) {
+//   table = tables[i];
+
+//   if (thead = table.querySelector("thead")) {
+//       headers = thead.querySelectorAll("th");
+
+//       for (j = 0; j < headers.length; j++) {
+//           headers[j].innerHTML = "<a href='#'>" + headers[j].innerText + "</a>";
+//       }
+
+//       thead.addEventListener("click", sortTableFunction(table));
+//   }
+// }
+
+// function sortTableFunction(table) {
+//   return function(ev) {
+//       if (ev.target.tagName.toLowerCase() == 'a') {
+//           sortRows(table, siblingIndex(ev.target.parentNode));
+//           ev.preventDefault();
+//       }
+//   };
+// }
+
+// function siblingIndex(node) {
+//   var count = 0 ;
+
+//   while (node = node.previousElementSibling) {
+//     count++ ;
+//   }
+//   return count;
+// }
+
+// function sortRows(table, columnIndex) {
+//   var rows = table.querySelectorAll("tbody tr"),
+//   sel = "thead th:nth-child(" + (columnIndex + 1) + ")",
+//   sel2 = "td:nth-child(" + (columnIndex + 1) + ")",
+//   classList = table.querySelector(sel).classList,
+//   values = [],
+//   cls = "",
+//   allNum = true,
+//   val,
+//   index,
+//   node ;
+
+//   if (classList) {
+//     if (classList.contains("date")){
+//       cls = "date" ;
+//     } else if (classList.contains("number")) {
+//       cls = "number" ;
+//     }
+//   }
+//   for (index = 0; index < rows.length; index++) {
+//     node = rows[index].querySelector(sel2);
+//     val = node.innerText ;
+//     if (isNaN(val)) {
+//       allNum = false ;
+//     } else {
+//       val = parseFloat(val);
+//     }
+//     values.push({value: val, row: rows[index] });
+//   }
+//   if (cls == "" && allNum) {
+//     cls = "number" ;
+//   }
+//   if (cls == "number") {
+//     values.sort(sortNumberVal);
+//     values = values.reverse();
+// } else if (cls == "date") {
+//     values.sort(sortDateVal);
+// } else {
+//     values.sort(sortTextVal);
+// }
+// for (var idx = 0; idx < values.length; idx++) {
+//   table.querySelector("tbody").appendChild(values[idx].row);
+// }
+// }
+
+// function sortNumber (a, b) {
+//   return a-b
+// }
+// function sortDateVal(a, b) {
+//   var dateA = Date.parse(a.value),
+//   dateB = Date.parse(b.value);
+//   return sortNumber(dateA, dateB);
+// }
+
+// function sortTextVal (a, b) {
+//   var textA = (a.value + "").toUpperCase();
+//   var textB = (b.value + "").toUpperCase();
+//   if (textA < textB) {
+//     return -1 ;
+//   }
+//   if (textA > textB) {
+//     return 1;
+//   }
+//   return 0;
+// }
+
 // console.logI(eventSearchSG);
 // console.log(eventSearchTM);
 
@@ -55,8 +155,9 @@ html`<div id="search-bar">
 <input type="search" class="event-search" name="event-input" placeholder="Search team, artist, location, or date" />
 </div>
 <div id="results">
-  <table id="resultsTable">
-      <tr id="headersRow">
+  <table id="resultsTable" class="table-sortable">
+    <thead>
+    <tr id="headersRow">
         <th id="column-one">Date & Time</th>
         <th id="column-two">Location</th>
         <th id="column-three">Event</th>
@@ -64,7 +165,9 @@ html`<div id="search-bar">
         <th id="column-five">Vendor</th>
         <th id="column-six">Price</th>
       </tr>
-      <div id="apiResults">
+    </thead>
+    <tbody>
+    <div id="apiResults">
       ${state.eventSearchTM
   .map(Events => {
     return `
@@ -78,6 +181,7 @@ html`<div id="search-bar">
     `
   }).join("")}
       </div>
+    </tbody>
   </table>
   </div>
   <div class="bottom-of-search">
--- index.js
+++ index.js
@@ -70,7 +70,6 @@ function afterRender(state) {
   }
   if (state.view === "Events"){
     const input = document.querySelector('input[type="search"]');
-
     input.addEventListener("search", () => {
       console.log(input.value);
       let lowerCase = input.value.toLowerCase();
@@ -102,6 +101,38 @@ function afterRender(state) {
         // done();
       });
       router.navigate("/Events")
+  });
+  function sortTableByColumn(table, column, asc = true) {
+    const dirModifier = asc ? 1 : -1;
+    const tBody = table.tBodies[0];
+    const rows = Array.from(tBody.querySelectorAll("tr"));
+
+    const sortedRows = rows.sort((a, b) => {
+      const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+      const bColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+
+      return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+    });
+
+    while (tBody.firstChild) {
+      tBody.removeChild(tBody.firstChild);
+    }
+
+    tBody.append(...sortedRows);
+
+    table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
+    table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-asc", asc);
+    table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-desc", !asc);
+  }
+
+  document.querySelectorAll(".table-sortable th").forEach(headerCell => {
+    headerCell.addEventListener("click", () => {
+      const tableElement = headerCell.parentElement.parentElement.parentElement;
+      const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+      const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+
+      sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
+    });
   });
   }
   if (state.view === "Contact") {
